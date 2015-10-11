c79d403953afd5e9e5fc8ce3d0388a9c94ac5428
as335 <icm0770@gmail.com>
2015-10-11 19:25:44 -0400

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,4 @@
+project1<-read.table("hpc070201070202.txt", sep=";",header=TRUE)
+head(project1)
+
+hist(project1$Global_active_power, xlab="Global Active Power (kilowatts)", col="red", main="Global Active Power")
