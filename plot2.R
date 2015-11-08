4ffef2d31ea87b92ed2a4b6b3ba260542ea0a7eb
as335 <icm0770@gmail.com>
2015-11-08 18:34:11 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,13 @@
+project1<-read.table("hpc070201070202.txt", sep=";",header=TRUE)
+head(project1)
+
+project1$Date<-as.Date(project1$Date, format = "%d/%m/%Y")  
+project1$timetemp<-paste(project1$Date, project1$Time)  
+project1$Time<-strptime(project1$timetemp, format = "%Y-%m-%d %H:%M:%S")  
+project1$weekdays<-weekdays(as.Date(project1$Date))
+
+globalactivepower<-as.numeric(project1$Global_active_power)
+
+png('plot2.png')
+plot(project1$Time, globalactivepower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
