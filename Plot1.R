ce8c3da786a2bed3bf50c869a31f50dfb49ea879
as335 <icm0770@gmail.com>
2015-11-08 18:32:14 -0500

    Delete Plot1.R

--- Plot1.R
+++
@@ -1,4 +0,0 @@
-project1<-read.table("hpc070201070202.txt", sep=";",header=TRUE)
-head(project1)
-
-hist(project1$Global_active_power, xlab="Global Active Power (kilowatts)", col="red", main="Global Active Power")
